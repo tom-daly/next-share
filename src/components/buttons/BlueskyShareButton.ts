bf600d21fd1b7dda7ba6135ea1a39c7fa80db194
Thomas Daly <tom.m.daly@gmail.com>
2025-01-29 15:23:21 -0500

    remove extra functions

--- src/components/buttons/BlueskyShareButton.ts
+++ src/components/buttons/BlueskyShareButton.ts
@@ -4,37 +4,25 @@ import createShareButton from '../../hocs/createShareButton';
 function blueskyLink(
   url: string,
   {
-    title,
-    via,
-    hashtags = [],
-    related = [],
-  }: { title?: string; via?: string; hashtags?: string[]; related?: string[] },
+    title
+  }: { title?: string; },
 ) {
   return (
     'https://bsky.app/intent/compose' +
     transformObjectToParams({
       url,
-      text: title,
-      via,
-      hashtags: hashtags.length > 0 ? hashtags.join(',') : undefined,
-      related: related.length > 0 ? related.join(',') : undefined,
+      text: title
     })
   );
 }
 
 const BlueskyShareButton = createShareButton<{
   title?: string;
-  via?: string;
-  hashtags?: string[];
-  related?: string[];
 }>(
   'bluesky',
   blueskyLink,
   (props) => ({
-    hashtags: props.hashtags,
     title: props.title,
-    via: props.via,
-    related: props.related,
   }),
   {
     windowWidth: 550,
